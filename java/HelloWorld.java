48baf3d95ef04bc937440cfcfb1ded59c97b613c
Chawin Aiemvaravutigul <nick41746@hotmail.com>
2018-10-03 23:23:32 +0700

    add my_hello_world.cpp

--- java/HelloWorld.java
+++ java/HelloWorld.java
@@ -1,5 +1,6 @@
-public class HelloWorld{
-    public static void main(String[] args) {
-		  System.out.println("Hello World");
-    }           
+public class Hello
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World !!");
+	}
 }
