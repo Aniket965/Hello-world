41b7b91947cca313618ec1060f1ffded44fc9df5
Kevin Boer <kevin@keytoe.nl>
2018-10-03 17:48:32 +0200

    C# hello world

---
+++ C#/hello.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+
+            
+        }
+    }
+}
