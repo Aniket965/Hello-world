63f221f36e900cd608cab8a3a22d6b21109283f7
PARASPECIALFORCES <43858927+PARASPECIALFORCES@users.noreply.github.com>
2018-10-04 23:18:59 +0530

    Add files via upload

---
+++ Python/Counting the Number words.py
@@ -0,0 +1,6 @@
+def main():
+    s = input("Please enter your sentence: ")
+    words = s.split()
+    wordCount = len(words)
+    print ("Your word and letter counts are:", wordCount)
+main()
---
+++ Python/Counting the occurence of each word.py
@@ -0,0 +1,11 @@
+string=input("Enter string:")
+word=input("Enter word:")
+a=[]
+count=0
+a=string.split(" ")
+for i in range(0,len(a)):
+      if(word==a[i]):
+            count=count+1
+print("Count of the word is:")
+print(count)
+
