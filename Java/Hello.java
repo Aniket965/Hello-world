7d4d9806e0f522851e542784aa3e766f1f3fb7d6
Maxxxxxx2point0 <42625653+Maxxxxxx2point0@users.noreply.github.com>
2018-10-04 19:21:31 -0600

    ok

--- Java/Hello.java
+++ Java/Hello.java
@@ -1,6 +1,6 @@
-public class Hello{
-    public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
-        System.exit( 0 ); //success
-    }
+public class Hello
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World !!");
+	}
 }
