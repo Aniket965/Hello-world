b8c73990013776373ad9368b7312cea0c36bbac9
anuragk56 <anurag.189302059@muj.manipal.edu>
2018-10-05 23:42:52 +0530

    program to find average of 3 score

---
+++ Python/Average_for_3test.py
@@ -0,0 +1,12 @@
+# Get three test score
+round1 = int(raw_input("Enter score for round 1: "))
+
+round2 = int(raw_input("Enter score for round 2: "))
+
+round3 = int(raw_input("Enter score for round 3: "))
+   
+# Calculate the average
+average = (round1 + round2 + round3) / 3
+
+# Print out the test score
+print "the average score is: ", average
