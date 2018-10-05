addd47385be21da609218f98df13e0cd7491b478
jkunal14 <43882559+jkunal14@users.noreply.github.com>
2018-10-05 22:41:56 +0530

    Update add2nos.cpp

--- C++/add2nos.cpp
+++ C++/add2nos.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+using namespace std;
 
 int main()
 {
-   int x,y;
+   	double x,y;
+	cout<<"Enter the numbers you wish to add"<<endl;
 	cin>>x>>y;
- 	cout<<x+y;
- getch();
+ 	cout<<"The sum of numbers is "<<x+y<<endl;
+	return 0;
 }
