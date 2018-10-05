114a6077168e34ff5bcea8eb9d9d730498df499f
Antz Shrek <antzshrek@localhost.localdomain>
2018-10-05 08:32:48 +0100

    hello_world

---
+++ C/antz.c
@@ -0,0 +1,19 @@
+/*
+
+     prime.c
+     Finds prime numbers using an iterative method.
+
+     This program is a first push for the Hacktoberfest-2018
+
+     Written by Reuben Antz.  (10-05-2018)
+
+*/
+
+#include <stdio.h>
+
+int main() {
+    
+    // Print function
+    printf("\nHello World \n\tfrom Antz!\n");
+    return 0;
+}
