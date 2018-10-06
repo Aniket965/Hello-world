e022b2f245d43fd5a81c45123ed39e1ec2a007af
Saranya-jena <saranyajena29@gmail.com>
2018-10-06 20:21:52 +0530

    saranya_hack

--- Swift/hello.swift
+++
@@ -1,5 +0,0 @@
-import UIKit
-import Foundation
-let message: String = "Hello, World!"
-print(message, terminator: "") 
-fileManager.createFile(path:"/", contents: message, attributes:[FileAttributeKey : Any]? = nil)
--- helloworld.cpp
+++ helloworld.cpp
@@ -2,6 +2,6 @@
 using namespace std;
 
 int main() {
-    printf("Hello, World.");
+    printf("Hello, World!");
     return 0;
 }
