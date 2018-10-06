e4ebdb78f1f249d2afcfea8567fdef613d28714c
phanirithvij <phanirithvij20003@gmail.com>
2018-10-06 11:55:51 +0530

    Python simple join, string add, string multiplication examples

---
+++ Python/funny_Hello.py
@@ -0,0 +1,7 @@
+# Join method explained "".join(list)
+# String multiplication explained "l"*2
+# String concat explained str + str
+
+he = ''.join(["H","e","l"*2,"o"])
+wo = ''.join(["W","o","r","l","d"])
+print(he + " " + wo)
