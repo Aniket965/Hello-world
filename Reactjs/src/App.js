d0ea6f783bf6cc18fc6b6aad4f8b7e05458fa36f
Kim Morano <ksmorano@gmail.com>
2018-10-06 22:53:37 +0800

    updated App.js added a button that toggles background color

--- Reactjs/src/App.js
+++ Reactjs/src/App.js
@@ -1,19 +1,44 @@
 // Importing react modules
 
 import React, { Component } from "react";
-import ducky from "././images/HalloweenDuck.jpg";
+import brownPumpkin from "./images/pumpkin-brown.png";
+import orangePumpkin from "./images/pumpkin-orange.png";
 import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isDark: false
+    };
+  }
+
+  toggleDark = () => {
+    this.setState(previousState => {
+      return { isDark: !previousState.isDark };
+    });
+  };
   render() {
     return (
-      <div className="App">
+      <div
+        className="App"
+        style={{ backgroundColor: `${this.state.isDark ? "black" : "orange"}` }}
+      >
         <header className="App-header">
-          <img src={ducky} className="pumpkin" alt="pumpkin duck" />
-          <p>Hello World! Quack!</p>
+          <p style={{ color: `${this.state.isDark ? "white" : "black"}` }}>
+            Hello World! Happy Halloween!
+          </p>
+          <img
+            className="logo"
+            src={this.state.isDark ? orangePumpkin : brownPumpkin}
+            alt=""
+          />
         </header>
 
-        <footer>
+        <footer className="footer">
+          <button className="button" onClick={this.toggleDark}>
+            Turn {this.state.isDark ? "on" : "off"} the lights
+          </button>
           created by <a href="https://github.com/CQW-Code"> CQW-Code</a>
         </footer>
       </div>
