b132425244942a85a016fcf016a99962da020e1b
alex <alex_koh_@live.com>
2018-10-07 02:56:18 +0800

    Fixed logical bug in LeapYear

--- Java/LeapYear.java
+++ Java/LeapYear.java
@@ -2,21 +2,18 @@ import java.util.Scanner;
 
 public class LeapYear{
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
         
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Enter a Year : ");
-        int year = scan.nextInt();
+    Scanner scan = new Scanner(System.in);
+    System.out.print("Enter a Year : ");
+    int year = scan.nextInt();
 
-        if(year%4==0 || year%100==0)
-            {
-                System.out.println("Yes it is a Leap year");
-            }
-        else
-            {
-                System.out.println("No it is not a Leap year");
-            }
-
+    if (year % 4 == 0 && year % 100 != 0) {
+      System.out.println("A leap year");
+    } else if (year % 400 == 0) {
+      System.out.println("A leap year");
+    } else {
+      System.out.println("Not a leap year");
     }
-
+  }
 }
