97fd12fdd7e92bcad33af6c6a6d2225882e3a4a1
Narendrareddys <43902946+Narendrareddys@users.noreply.github.com>
2018-10-06 13:53:37 +0530

    Update Palidrome.cpp

--- C++/Palidrome.cpp
+++ C++/Palidrome.cpp
@@ -1,18 +1,21 @@
 #include <iostream>
-
 using namespace std;
-
+int palindrome(int n)
+{
+     while( n > 0){
+    a = n % 10;
+    n = n / 10;
+    s = s * 10 + a;
+    }
+    return s;
+}
 int main()
 {
     int a,n,b,s=0;
     cout << "Please enter a number" << endl;
     cin >> n;
     b = n;
-    while( n > 0){
-    a = n % 10;
-    n = n / 10;
-    s = s * 10 + a;
-    }
+   s=palindrome(n);
     if( b == s){
     cout << "Yes" << endl;
     }
