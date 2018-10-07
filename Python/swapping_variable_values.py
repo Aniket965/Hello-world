b469caa7d10a011f8d11276ec89c76097590e41d
vivekjagannath <vivekj1203@gmail.com>
2018-10-07 17:42:37 +0530

    swap variables

---
+++ Python/swapping_variable_values.py
@@ -0,0 +1,12 @@
+#We take 2 variables with certain values:
+a= 10
+b= 20
+
+# Swapping the values of the variables:
+b= a + b
+a= b - a
+b= b - a
+
+# Printing out to check the values: 
+print a
+print b
