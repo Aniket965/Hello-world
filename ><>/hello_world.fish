9ba0e78327ffda21c64e655a4a8aafae35cfd841
Ashmita08 <42461605+Ashmita08@users.noreply.github.com>
2018-10-07 08:27:39 +0530

    Update hello_world.fish

--- ><>/hello_world.fish
+++ ><>/hello_world.fish
@@ -1,2 +1,15 @@
-"Hello world!"r\
-          o;!?l<
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Hactberfest</title>
+</head>
+<body>
+<h1><em><center><u>HELLO  WORLD!</u></center></em></h1>
+<p>
+   <h2><strong>Hactoberfest'18 </strong></h2>
+</p>
+</body>
+</html>
