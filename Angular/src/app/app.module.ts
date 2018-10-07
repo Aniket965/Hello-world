be14589053089e5cdb25bf27d11b619a05fe4e56
Nickx58 <nikhilx58@gmail.com>
2018-10-07 11:18:43 +0530

    Added Reactive Form

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
