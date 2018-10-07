9ea81bb99af3985c1065ccedd0acabd53e175e1c
ruwan liyanage <30474275+ruwanliyanage123@users.noreply.github.com>
2018-10-08 00:36:16 +0530

    remake for take user inputs

--- Java/HelloWorld/src/HelloWorld.java
+++ Java/HelloWorld/src/HelloWorld.java
@@ -1,11 +1,12 @@
-
+import java.util.Scanner;
 public class HelloWorld {
-
 	public static void main(String[] args) {
-	// Print Hello World
-        System.out.println("Hello, World");
+		// get usesr input for display
+        	Scanner input = new Scanner(System.in);
+		String in = input.nextLine();
+		System.out.println(in);
     }
 
-	}
+}
 
 
