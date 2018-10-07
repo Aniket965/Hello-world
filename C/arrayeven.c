22bf9f2a4462911f8781ca1e20cd434bea88ceb2
sakigo9 <43814993+sakigo9@users.noreply.github.com>
2018-10-07 18:02:51 +0530

    change in for loop

--- C/arrayeven.c
+++ C/arrayeven.c
@@ -4,25 +4,21 @@ int main(){
 
     // Print even and odd numbers stored in the array
     int arr1[10];
-    int i=0,evensum=0,oddsum=0,size=0;
+    int i=0,evensum=0,size;
 
     printf("Enter the size of the array ");
     scanf("%d",&size);
 
     printf("\nEnter %d elements in the array\n",size);
-    for(i=0;i<size;i++){
+    for(i=1;i<=size;i++){
         scanf("%d",&arr1[i]);
         if(arr1[i]%2==0)
         {
-            // Even Element
             evensum = evensum + arr1[i];
         }
-        else{
-            // Odd Element
-            oddsum = oddsum + arr1[i];
-        }
+       
     }
-    printf("Sum of even elements of the array is %d\nSum of Odd elements of the array is %d",evensum,oddsum);
+    printf("Sum of even elements of the array is %d\n",evensum);
 
     printf("\n");
     return 0;
