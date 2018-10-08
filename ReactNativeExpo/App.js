5fa6d3b0b5b1816193e7ba21c9bd73820a8b1217
boat46450 <boat46450@hotmail.com>
2018-10-08 23:54:26 +0700

    change text to hello world

--- ReactNativeExpo/App.js
+++ ReactNativeExpo/App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Hello World</Text>
       </View>
     );
   }
