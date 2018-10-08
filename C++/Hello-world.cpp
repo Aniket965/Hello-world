f18008197b22d1e4230227451558c31584e94f6b
prince17080 <40901104+prince17080@users.noreply.github.com>
2018-10-08 22:59:28 +0530

    Update Hello-world.cpp

--- C++/Hello-world.cpp
+++ C++/Hello-world.cpp
@@ -1,7 +1,20 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
+
 int main()
 {
-cout<<"HELLO WORLD";
-return 0;
+	cout<<char('A' + 7);
+	cout<<char('a' + 4);
+	cout<<char('a' + 11);
+	cout<<char('a' + 11);
+	cout<<char('a' + 14);
+	cout<<char(32);
+	cout<<char('A' + 22);;
+	cout<<char('a' + 14);
+	cout<<char('a' + 17);
+	cout<<char('a' + 11);
+	cout<<char('a' + 3);
+	cout<<char(33);
+	cout<<endl;
 }
