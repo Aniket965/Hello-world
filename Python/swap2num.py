e67ce0a57f0c27377e153d3feb6b419e7d26d579
roncyrthomas <roncy516@gmail.com>
2018-10-08 23:08:22 +0530

    added new program

---
+++ Python/swap2num.py
@@ -0,0 +1,7 @@
+n1=int(input("Enter the first number:-  "));
+n2=int(input("Enter the second number:- "));
+print("original numbers, A= ",n1,", B= ",n2);
+n1=n1+n2;
+n2=n1-n2;
+n1=n1-n2;
+print("swapped numbers, A= ",n1,", B= ",n2);
