d9f42e658744d02eea0172ce1e822b7f4be60dfe
saurabh <38917181+saurabhpetkar@users.noreply.github.com>
2018-10-09 19:30:33 +0530

    Update triangle.cpp

--- C++/triangle.cpp
+++ C++/triangle.cpp
@@ -6,8 +6,13 @@ using namespace std;
 int main()
 {
     int n,k,l=-1,r;
-    cout<<"enter the value of rows:\n";
-    cin>>n;
+    cout<<"enter the value of rows(above 0):\n";
+	while(1){
+		cin>>n;
+		if(n>0)
+			break;
+		cout<<"make sure that the rows is more than 0\n\n";
+	}
 
     for(int i=1;i<=n;i++)
     {
