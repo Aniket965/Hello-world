96c3fe67acb0a6a7cdd0a860d09ba59a1fc41773
Aditya Jeswani <adit98@gmail.com>
2018-10-09 21:35:49 +0530

    Changes in formatting to naive_string.py

--- Python/naive_string.py
+++ Python/naive_string.py
@@ -2,27 +2,29 @@
 
 text = input('Enter the text to search in: ')
 text = text.lower()
+
 pattern = input('Enter pattern to search for: ')
 pattern = pattern.lower()
+
 indexes = []
 n = len(text)
 m = len(pattern)
 
-for i in range(n-m+1):
+for i in range(n - m + 1):
 	if text[i] == pattern[0]:
 		
 		j = 0
 		while j < m:
-			if pattern[j] != text[j+i]:
+			if pattern[j] != text[j + i]:
 				break
 			j = j + 1
 		if j == m:
-			indexes.append(i+1)
+			indexes.append(i + 1)
 
 if indexes:
-	print('The Pattern occurs at position:',end=' ')
+	print('The Pattern occurs at position:', end = ' ')
 	for i in indexes:
-		print(i, end=', ')
+		print(i, end = ', ')
 	print()
 else:
 	print('Pattern Not Found!')
