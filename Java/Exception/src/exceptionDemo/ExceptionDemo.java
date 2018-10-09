329fd995957b4b2ec8c1b255c3f862fba4938a3c
harshamjayz <harshamjayz@gmail.com>
2018-10-09 22:13:31 +0530

    exeption

---
+++ Java/Exception/src/exceptionDemo/ExceptionDemo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exceptionDemo;
+
+/**
+ *
+ * @author badhr
+ */
+public class ExceptionDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a = Integer.parseInt("100a");
+        //Thread.sleep(100);
+        
+    }
+    
+}
