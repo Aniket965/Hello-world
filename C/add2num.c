d91da38cad0088646a36a4b8eace65888d49484e
Augustine S Aykara <augustineaykara@gmail.com>
2018-10-10 00:25:14 +0530

    update add2num.c
    let value to be inserted by user

--- C/add2num.c
+++ C/add2num.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
-
-
 int main(){
-
     int a,b;
-    a=5;b=10;
-
+    printf(" enter two numbers : ");
+    scanf("%d %d", &a,&b);
     printf("The addition of two numbers is %d",a+b);
     printf("\n");
     return 0;
