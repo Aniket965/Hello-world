bab10e685a987498a4a56269348f0c07530deab7
Tyler Phillips <thetylerjp@icloud.com>
2018-10-09 10:43:46 -0400

    helloWorldLabel: Swift

--- C++/Hello_World.cpp
+++ C++/Hello_World.cpp
@@ -1,7 +1,7 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 int main()
 {
-cout<<"Hello World"<<"\n";
-return 0;
+    cout<<"Hello World\n";
+    return 0;
 }
--- Swift/hello.swift
+++ Swift/hello.swift
@@ -1,1 +1,20 @@
+class HelloWorldSwift: UIViewController {}
+
 print("Hello, World!")
+
+
+    let helloWorldLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Hello World!!!"
+        label.isUserInteractionEnabled = false
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+      override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.addSubview(helloLabel)
+      }
+
+}
