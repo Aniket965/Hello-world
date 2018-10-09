3740ee19f2f6bd8ab6fecfea21130511291b4d7f
VictoriaLasso <cajavi90@hotmail.com>
2018-10-09 14:48:26 -0500

    Create Hello  world
    Create hello world of java with joption pane

---
+++ Java/helloWorld.java
@@ -0,0 +1,17 @@
+package helloWorld;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author VictoriaLasso
+ */
+public class helloWorld {
+
+    public static void main(String[] args) {
+        
+        imprimir+="\n Hello World";
+        JOptionPane.showMessageDialog(null, imprimir);
+    }
+    
+}
