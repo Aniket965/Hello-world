b13549f72d56c6ca82fb2cb98003d7aefe70540d
aman566 <amangupta566@gmail.com>
2018-10-09 18:26:50 +0530

    HelloWorld Program

---
+++ helloWorld.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	int i;
+	printf("Enter any number to print hello world");
+	int j=scanf("%d",&i);
+	printf("The number is: %d",j);
+	printf("Welcome to the worlld of C programming");
+}
