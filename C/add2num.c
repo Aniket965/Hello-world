a96ddb2920fbebf2bf86d70f80d6ba68b8980cdf
Augustine S Aykara <augustineaykara@gmail.com>
2018-10-10 00:27:18 +0530

    update add2num.c
    reduced no. of lines of code

--- C/add2num.c
+++ C/add2num.c
@@ -1,14 +1,8 @@
 #include <stdio.h>
-
-
 int main(){
-
     int a,b;
-    int sum;
     a=5;b=10;
-    sum=a+b;
-
-    printf("The addition of two numbers is %d",sum);
+    printf("The addition of two numbers is %d",a+b);
     printf("\n");
     return 0;
 }
