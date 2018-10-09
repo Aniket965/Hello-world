06b0506503d2772ac8f2b60d3513d496ee63c5d1
Benny Schreck <benny.schreck@kanzlei-jacobs.de>
2018-10-09 11:59:02 +0200

    something added

--- Java/HelloWorld/src/hello_world.java
+++ Java/HelloWorld/src/hello_world.java
@@ -3,6 +3,9 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello_World");
+
+
+        System.out.println("I*m in your code")
     }
 
 }
