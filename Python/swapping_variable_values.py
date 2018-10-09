e45c83c990d4b3d09e69f3da5cb89247466b3ee6
gdivya17 <33058649+gdivya17@users.noreply.github.com>
2018-10-09 22:02:00 +0530

    Update swapping_variable_values.py

--- Python/swapping_variable_values.py
+++ Python/swapping_variable_values.py
@@ -1,11 +1,11 @@
 #We add any two variables with certain values:
-a= int(raw_input("enter any  number"))
-b= int(raw_input("enter any number"))
+a= int(input("enter a"))
+b= int(input("enter b"))
 # Swapping the values of the variables:
 b= a + b
 a= b - a
 b= b - a
 
 # Printing out to check the values: 
-print a
-print b
+print(a)
+print(b)
