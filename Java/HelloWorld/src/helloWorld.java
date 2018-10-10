addea2a5a5366b0de8017164cc4b05dc579c4a44
duydosomething <dugtony@gmail.com>
2018-10-09 19:16:35 -0700

    commited hello fren

--- Java/HelloWorld/src/helloWorld.java
+++
@@ -1,8 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter your name!");
-        String name=sc.nextLine();
-        System.out.println("hey!"+name+", welcome to the world of java.");
-    }
-}
