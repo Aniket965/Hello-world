3cb3a11a39ea57e6365a6a20b0bf709a5883b868
EricMunoz <munoz.eric.d@gmail.com>
2018-10-09 21:56:20 -0500

    Add files via upload

--- Java/HelloWorld.java
+++ Java/HelloWorld.java
@@ -1,6 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello.world;
 
+/**
+ *
+ * @author ericmunoz
+ */
 public class HelloWorld {
-	public static void main(String[] args) {
-		System.out.println("Hello, World");
-	}
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello, World!");
+    }
+    
 }
