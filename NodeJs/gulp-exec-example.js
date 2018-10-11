ebee26e2c26f35ff21e1b3e785afdf13a68f023c
Mrugesh Vaghela <mvaghela@roambee.com>
2018-10-11 14:48:03 +0530

    added console for successful execution

--- NodeJs/gulp-exec-example.js
+++ NodeJs/gulp-exec-example.js
@@ -4,6 +4,7 @@ gulp.task('task', function (cb) {
   exec('ping localhost', function (err, stdout, stderr) {
     console.log(stdout);
     console.log(stderr);
+    console.log('Task executed successfully.');
     cb(err);
   });
 })
