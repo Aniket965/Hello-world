5e7021dcc1735499a90b0f322149f9fe5393fd79
Jonas <Jonasdero@users.noreply.github.com>
2018-10-11 13:23:55 +0200

    Update app.component.ts

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Hacktoberfest-2018';
+  title = 'Hacktoberfest-2018 (Hello World in every Language!)';
   hackForm: FormGroup;
 
   constructor() {
