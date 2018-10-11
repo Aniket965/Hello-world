f65ecfc79726b8172d9c66033b5df1cda49d9f07
Alex Jacobs <alexander.jacobs@myy.haaga-helia.fi>
2018-10-11 20:20:25 +0300

    added basic error handling to my greeter file

--- Javascript/greeter.js
+++ Javascript/greeter.js
@@ -1,5 +1,10 @@
 function greeter(name) {
-	console.log(`Hey there ${name}! I hope you have a great day today!`);
+  if (typeof name === "string") {
+    console.log(`Hey there ${name}! I hope you have a great day today!`);
+  }
+  else {
+    console.log("Oops, looks like you didn't pass me a string!");
+  }
 }
 
 console.log(greeter("Alex"));
