e40d4c4383411acaebc838a5aa92681adc19c4c3
Raju Kumar <raju.raw.zoo@gmail.com>
2018-10-12 17:23:44 +0530

    create ipaddress.java
    added new file in java for ipaddress

---
+++ Java/ipaddress.java
@@ -0,0 +1,7 @@
+import java.net.InetAddress;
+//GET IP ADDRESS of CURRENT NETWORK
+class ipaddress {
+  public static void main(String arg[]) throws Exception {
+	    System.out.println(InetAddress.getLocalHost());
+		 }
+	}
