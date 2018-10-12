db4d82f73c243da5f6cb8d7a9ae299f3e83b0286
Saurabh Shukla <saurabh.shukla244@gmail.com>
2018-10-12 17:38:35 +0530

    Create nCr%p_lucas_theorem.cpp

---
+++ nCr%p_lucas_theorem.cpp
@@ -0,0 +1,41 @@
+// A Lucas Theorem based solution to compute nCr % p 
+#include<bits/stdc++.h> 
+using namespace std; 
+  
+
+int nCrModpDP(int n, int r, int p) 
+{ 
+  
+    int C[r+1]; 
+    memset(C, 0, sizeof(C)); 
+  
+    C[0] = 1; 
+  
+   
+    for (int i = 1; i <= n; i++) 
+    { 
+        
+        for (int j = min(i, r); j > 0; j--) 
+  
+            // nCj = (n-1)Cj + (n-1)C(j-1); 
+            C[j] = (C[j] + C[j-1])%p; 
+    } 
+    return C[r]; 
+} 
+
+int nCrModpLucas(int n, int r, int p) 
+{ 
+   
+   if (r==0) 
+      return 1; 
+ 
+   int ni = n%p, ri = r%p;
+   return (nCrModpLucas(n/p, r/p, p) * nCrModpDP(ni, ri, p)) % p; 
+} 
+  
+int main() 
+{ 
+    int n = 1000, r = 900, p = 13; 
+    cout << "Value of nCr % p is " << nCrModpLucas(n, r, p); 
+    return 0; 
+} 
