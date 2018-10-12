e5a012d387e8bf3797f8281d73c949f711c09adc
IronLaddle <abdtasnim0@gmail.com>
2018-10-12 15:27:44 +0800

    format date to dd-MM-yyyy

--- Java/DateDemo.java
+++ Java/DateDemo.java
@@ -1,4 +1,5 @@
 import java.util.Date;
+import java.text.SimpleDateFormat;
 public class DateDemo {
 
    public static void main(String args[]) {
@@ -7,5 +8,10 @@ public class DateDemo {
 
       // display time and date using toString()
       System.out.println(date.toString());
+      
+      //format date to day-month-year
+      SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+	   String format = formatter.format(date);
+	   System.out.println(format);
    }
 }
