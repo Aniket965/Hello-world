e20457337c55dec156d6ca617807e12a7a7d8e3f
bin senpai <me@bin.moe>
2018-10-11 21:58:13 -0400

    Create WeebHelloWorld.cs

---
+++ C#/WeebHelloWorld.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Japan
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      Console.WriteLine("こんにちわ!");
+      Console.ReadKey();
+    }
+  }
+}
