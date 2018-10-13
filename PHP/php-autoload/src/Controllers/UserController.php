574701bad11504f25eecbc5120ae0fc1c9b49ecc
abdullahaaf <abdullahaaf1@gmail.com>
2018-10-13 10:14:40 +0700

    add implementation of autoloading that follow PSR-4

---
+++ PHP/php-autoload/src/Controllers/UserController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: abdullahaaf
+ * Date: 30/09/18
+ * Time: 8:32
+ */
+namespace App\Controllers;
+
+class UserController
+{
+    public function index()
+    {
+        echo "Ini adalah output dari class UserController method index \n";
+    }
+}
+?>
