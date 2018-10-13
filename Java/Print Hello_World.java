42ef139c3d1c8098b1dbb63b0e0e3f72b6d09636
Geriththanan Naguleswaran <43606256+Geriththanan@users.noreply.github.com>
2018-10-13 06:59:05 +0530

    Create Print Hello_World.java

---
+++ Java/Print Hello_World.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
