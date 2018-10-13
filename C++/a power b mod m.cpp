655b5fb097ea4c7ac5235b4b9f91cef9a3f9c6d3
vivekreddy2 <padigelavivekreddy@gmail.com>
2018-10-13 20:15:05 +0530

    added comments

--- C++/a power b mod m.cpp
+++ C++/a power b mod m.cpp
@@ -2,9 +2,11 @@
 using namespace std;
 #define ll long long
 long long power_mod_p(ll a, ll b, ll m){
-
+	
+		// if power is zeoro return 1
 		if(!b)return 1;
-
+	
+		//otherwise call a^2 power b/2 recursively
 		return power_mod_p(a*a%m, b/2, m)*(b&1? a:1)%m;
 	}
 
