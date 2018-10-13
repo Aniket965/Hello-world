279e88fafc52bf93f80a4a4d6b2b61f7540c2a41
Kamila Malanowicz <kamila.malanowicz@bccgroup.com>
2018-10-13 18:40:39 +0200

    Add Java Hello World From Poland

---
+++ Java/HelloFromPoland.java
@@ -0,0 +1,8 @@
+public class HelloFromPoland {
+
+    public static void main(String[] args) {
+        // Prints "Hello from Poland!" to the terminal window.
+        System.out.println("Hello from Poland!");
+    }
+
+}
