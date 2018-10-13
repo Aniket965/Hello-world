55e148aebeab24561c6d4ac6d84da3be555d0ff8
Anh Nguyen <anhnguyen@Anh-Nguyen-MacBook.local>
2018-10-13 18:39:16 +0700

    Hello hacktoberfest

---
+++ Java/HelloHacktoberfest.java
@@ -0,0 +1,8 @@
+public class HelloHacktoberfest {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
