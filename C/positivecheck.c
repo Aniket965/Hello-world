c4734b84e666411156578cc10fa9a9d9ca0fec0e
segozcan <44121605+segozcan@users.noreply.github.com>
2018-10-13 21:48:10 +0300

    Update positivecheck.c

--- C/positivecheck.c
+++ C/positivecheck.c
@@ -2,23 +2,23 @@
 
 int main(){
 
-    int num=0;
+    int num;
     printf("Enter a Number to check \n");
     scanf("%d",&num);
 
     if(num>0)
     {
-        printf("Number is positive");
+        printf("Number is a positive number.");
     }
     if(num==0)
     {
-        printf("Number is non Negative nor positive");
+        printf("Number is neither negative nor positive.");
     }
     else
     {
-        printf("Number is a negative Number");
+        printf("Number is a negative number.");
     }
 
-    printf("\n");
+  
     return 0;
 }
