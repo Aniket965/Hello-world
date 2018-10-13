4fde180946af5f49f67796f625511ee8e4c71594
ayushdas99 <ayushd99@gmail.com>
2018-10-13 18:53:29 +0530

    Program to find square root of a number.

---
+++ C++/Square-root.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int t,r;
+    cin >> t;
+    for(int i=0; i<t; i++){
+    int a;
+    cin >> a;
+    r=sqrt(a);
+    cout << r << endl;
+    }
+
+    return 0;
+}
