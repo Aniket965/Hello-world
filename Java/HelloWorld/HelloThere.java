74bd15649be7469dcb1c99d47c3e612218aab5f2
pratik <pratikpalashikar@gmail.com>
2018-10-13 10:28:22 -0400

    Adding hello world program

---
+++ Java/HelloWorld/HelloThere.java
@@ -0,0 +1,10 @@
+package HelloWorld;
+
+public class HelloThere {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+
+        System.out.println("Hey there is pratikpalashikar");
+    }
+}
