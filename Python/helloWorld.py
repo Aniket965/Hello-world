97bfe425c50591f8d19bd1a766f85c9789e4fe09
João Laranjo <37157325+joaolaranjo@users.noreply.github.com>
2018-10-13 22:46:31 +0100

    helloWorld.py
    Simple Python3 program - Prints "Hello World!"

--- Python/helloWorld.py
+++ Python/helloWorld.py
@@ -1,1 +1,15 @@
-print("Hello World")
+# Simple Hello World Program
+# Author: João Laranjo
+# 2018-10-13
+
+# Python 3
+
+print("Hello World ! \n")
+
+# Another Hello World
+word = 'Hello World!'
+
+i = 0
+while i != len(word):
+    print(word[i])
+    i += 1
