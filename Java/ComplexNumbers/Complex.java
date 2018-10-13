b92f0658a501baf4256ccf0b6c41091fc83c0c97
Alex <acealex@brandeis.edu>
2018-10-13 11:38:06 -0400

    lists complex numbers

---
+++ Java/ComplexNumbers/Complex.java
@@ -0,0 +1,12 @@
+
+public class Complex {
+	public double re;
+	public double im;
+	
+	public String toString() {
+		return this.re+"+"+this.im+"i";
+	}
+	
+}
+
+
---
+++ Java/ComplexNumbers/TestComplex.java
@@ -0,0 +1,9 @@
+public static void main(String[] args) {
+	Complex z = new Complex();
+	z.re=1; z.im=2;
+	Complex x = new Complex();
+	w.re=3; w.im=1;
+	
+	System.out.println("z="+z);
+	
+}
