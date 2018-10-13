44569431c2c99d9cfda9b5caf7a53758b746e7d1
vivekreddy2 <padigelavivekreddy@gmail.com>
2018-10-13 15:36:52 +0530

    using inbuilt gcd function

--- C++/BuyingATVSet.cpp
+++ C++/BuyingATVSet.cpp
@@ -4,22 +4,12 @@ using namespace std;
 
 typedef unsigned long long int ulli; 
 
-ulli gcd(ulli x, ulli y){
-	if( x<0 ) x = -x;
-    if( y<0 ) y = -y;
-    while( y!=0 ){
-        x %= y;
-        if( x==0 ) return y;
-        y %= x;
-    }
-    return x;
-}
 
 int main() {
 	ulli a, b, x, y, gcdiv;
 	cin>>a>>b>>x>>y;
 	
-	gcdiv = gcd(x, y);
+	gcdiv = __gcd(x, y);
 	
 	x /= gcdiv;
 	y /= gcdiv;
