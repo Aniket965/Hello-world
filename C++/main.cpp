429442a0fc78c3fc96e028458a5456fb661534da
João Laranjo <37157325+joaolaranjo@users.noreply.github.com>
2018-10-14 00:45:03 +0100

    C++_HelloWorld
    C++ Program that prints Hello World!

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,34 +1,25 @@
-#include<iostream>
+//
+//  main.cpp
+//  helloWorld
+//
+//  Created by João Laranjo on 13/10/18.
+//  Copyright © 2018 João Laranjo. All rights reserved.
+//
+
+#include <iostream>
+
 using namespace std;
 
-void swap(int *a ,int *b)
-   { 
-	int temp=*a;
-      	*a=*b;
-      	*b=temp;
-   }
-
-void sort(int a[],int n)
-{
-	for (int round = 0; round < n-1; round++)
-		for (int i = 0; i < n-1- round; i++)
-		{
-      			if (a[i] > a[i + 1])
-      			swap(&a[i],&a[i+1]);
-		}
-}
-
-int main()
-{
-	int n;
-	cout<<"Enter the size of array: ";
-	cin>>n;
-	int arr[n];
-	cout << "Enter the array" << endl;
-	for (int i = 0; i < n; i++)
-		cin >> arr[i];
-	sort(arr,n);
-	cout<<"sorted array:"<<endl;
-	for (int i = 0; i < n; i++)
-		cout << arr[i] <<" ";
+int main(int argc, const char * argv[]) {
+    
+    cout << "Hello World!" << endl;
+    
+    string name = "Hello World!";
+    int i = 0;
+    
+    while (i != name.size()){
+        cout << name[i] << endl;
+        i++;
+    }
+    return 0;
 }
