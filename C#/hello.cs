7a1f90eec5f26a836991157e75dafe76478069e8
ammy001 <a.c.khairnar@gmail.com>
2018-10-14 10:33:43 +0530

    Update hello.cs

--- C#/hello.cs
+++ C#/hello.cs
@@ -7,9 +7,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
-            Console.ReadKey();
-
-            
+            Console.ReadKey();          
         }
     }
 }
