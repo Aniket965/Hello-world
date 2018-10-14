d217977dfe75a4d45cabdbd33fa1be9bb9ddca85
Elias Andrade <30844283+chaos4455@users.noreply.github.com>
2018-10-13 23:46:20 -0300

    Olá Mundo - Hello World c#

---
+++ C#/Olá Mundo c# Pt BR.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace OlaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo!");
+            Console.ReadKey();
+
+            
+        }
+    }
+}
