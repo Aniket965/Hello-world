4d986063976b8c6fa5911ee99e105c3949cc4496
Uvaish Zafri <35339873+uvaishzafri@users.noreply.github.com>
2018-10-14 22:32:24 +0530

    Update Factorial.cpp

--- C++/Factorial.cpp
+++ C++/Factorial.cpp
@@ -1,17 +1,22 @@
 #include<iostream>
 using namespace std;
-int factorial(int n)
-{
-    if(n==1 || n==0)
-        return 1;
-    else
-        return n*factorial(n-1);
-}
+
 int main()
 {
-    int n;
+    int n,fact;
     cout << "Enter a positive integer: ";
     cin >> n;
-    cout << "Factorial of " << n << " = " << factorial(n)<<endl;
+    fact=n;
+    if(n<=0)
+    {
+        cout<<"invalid number";
+        return 0;
+    }   
+    label:
+    fact*=(n-1);
+    n-=1;
+    if(n>0)
+        goto label;
+    cout << "Factorial of " << n << " = " << fact<<endl;
     return 0;
 }
