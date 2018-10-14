64c3398c34e1448819693b18eec1131c1e236839
LesnyRumcajs <LesnyRumcajs@users.noreply.github.com>
2018-10-14 11:36:14 +0200

    refactored to use STL

--- C++/Reverse a string.cpp
+++ C++/Reverse a string.cpp
@@ -1,28 +1,16 @@
 #include <iostream>
-#include<stdio.h>
-
-#include<string.h>
-
-using namespace std;
+#include <algorithm>
+#include <string>
 
 int main()
 {
- char str[100],temp;
- int i,j=0;
+ std::string str;
+ std::cout << "Enter any string :";
+ std::cin >> str;
 
- cout<<"Enter any the string :";
- cin >> str;
- i=0;
- j=strlen(str)-1;
-  while(i<j)
-   {
-   temp=str[i];
-   str[i]=str[j];
-   str[j]=temp;
-   i++;
-   j--;
-   }
- cout<<"Reverse string is: "<<str;
+ std::reverse(str.begin(), str.end());
 
-    return 0;
+ std::cout<< "Reverse string is: " << str << '\n';
+
+ return 0;
 }
