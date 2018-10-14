19692ec5a4f7ddf635b7984fe5281cfc84c1bcd8
William Zhou <wzhouwzhou@gmail.com>
2018-10-14 02:31:08 -0400

    Update hello-reverse.js
    Respect global 😩

--- NodeJs/hello-reverse.js
+++ NodeJs/hello-reverse.js
@@ -1,1 +1,1 @@
-Reflect.apply(console.log, 0, [(r = ((str = 'Hello, world!') => str.length === 0 ? str : `${r(str.substring(1))}${str[0]}`))()]);
+Reflect.apply(console.log, {}, [(this.r = ((str = 'Hello, world!') => str.length === 0 ? str : `${this.r(str.substring(1))}${str[0]}`))()]);
