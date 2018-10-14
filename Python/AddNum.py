470c6f9c70a750bf68c9a547f98e5f40dd722b1a
Pranay Anand <pranayanand123@gmail.com>
2018-10-15 01:19:39 +0530

    Update AddNum.py

--- Python/AddNum.py
+++ Python/AddNum.py
@@ -1,8 +1,6 @@
 # Adds two values
 num1 = input("Number 1: ")
 num2 = input("Number 2: ")
-
 sum = float(num1) + float(num2)
-
 # Displaying the addition result
 print("The sum of given numbers is: ", sum)
