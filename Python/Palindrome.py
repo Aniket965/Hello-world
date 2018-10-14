83f9625348aecbea9aab060e4060631ff214ef21
Pranay Anand <pranayanand123@gmail.com>
2018-10-15 01:07:53 +0530

    Update Palindrome.py

--- Python/Palindrome.py
+++ Python/Palindrome.py
@@ -1,12 +1,9 @@
-
 # function which return reverse of a string 
 def reverse(s): 
     return s[::-1] 
-  
 def isPalindrome(s): 
     # Calling reverse function 
     rev = reverse(s) 
-  
     # Checking if both string are equal or not 
     if (s == rev): 
         return True
