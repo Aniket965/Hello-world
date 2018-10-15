f0dc37f939532e2c2e9842827485cc867ec21fd5
Leeland Clenista <leeland_clenista@yahoo.com>
2018-10-14 19:55:44 -0700

    added node.js prime function

--- JavaScript/PrimeNumber.js
+++ JavaScript/PrimeNumber.js
@@ -1,6 +1,10 @@
+var readline = require('readline');
+
+
+
 // Function to check if a number is a prime number or not
 let primeNumber = function() {
-	let num = prompt('Enter a number to check')
+	let num = window.prompt('Enter a number to check')
 	for (var i = 2; i < num; ++i) {
 		if (num % i == 0)
       // Given Number is not a Prime Number.
@@ -10,5 +14,33 @@ let primeNumber = function() {
 	return 'Prime Number';
 }
 
-// Calling primeNumber and logging the result on console.
-console.log(primeNumber());
+//Function to check if a number is a prime number in Node.js
+let nodePrimeNumber = () => {
+	let rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout
+	});
+	
+	rl.question("Enter a number to check: ", function(num) {
+		// Converts input from string into number
+		let number=parseInt(num);
+		if(number===2){
+			console.log(`${number} is a Prime Number`) 
+		}
+		//console.log(typeof(num));
+		for (let i = 2; i < number; i++) {
+			if (number % i === 0) {
+				// Given Number is not a Prime Number.
+				console.log(`${number} is not a Prime number`);
+				return rl.close();
+			} else {
+				console.log(`${number} is a Prime Number`) 
+				return rl.close();
+			}
+		}
+		rl.close();
+	});
+}
+
+nodePrimeNumber();
+
