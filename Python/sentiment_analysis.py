4a4be7b4c035d7d032509f300e85f85425ff0bfb
luck-u <lakshaywadhwa001@gmail.com>
2018-10-15 22:19:20 +0530

    add python program

---
+++ Python/sentiment_analysis.py
@@ -0,0 +1,51 @@
+import tweepy
+from textblob import TextBlob
+import csv
+
+# Step 1 - Authenticate
+consumer_key= '1zGpcQ3ypVfghPdjeT6ITCN4U'
+consumer_secret= 'BJipbJ6Oe68ZI20iGxyPtkGGYk2wlp0PCphP6xKyCPesNoEXx5'
+
+access_token='814501931062267908-tpYgnnw9qQiTJ8hZbWy2lnWSPVQJCbQ'
+access_token_secret='C4SjuuNfj4jBuRzNkwvqnskBYJvvr1GUCHiNY9vbLDMlr'
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+#Step 3 - Retrieve Tweets
+public_tweets = api.search('@aamir_khan')
+file=open("tweet.csv","w")
+
+
+#CHALLENGE - Instead of printing out each tweet, save each Tweet to a CSV file
+#and label each one as either 'positive' or 'negative', depending on the sentiment 
+#You can decide the sentiment polarity threshold yourself
+
+
+for tweet in public_tweets:
+    
+    analysis=TextBlob(tweet.text)
+    print(analysis.sentiment)
+    if -1 < analysis.sentiment <=0 :
+        file.write(tweet.text.encode('utf-8'))
+        file.write("\n""negative")
+        with open('tweet.csv') as f:
+
+            print " ".join(line.strip() for line in f)  
+
+    
+        
+    else:
+        file.write(tweet.text.encode('utf-8'))
+        file.write("\n""positive")
+        with open('tweet.csv') as f:
+            print " ".join(line.strip() for line in f)  
+        
+        
+file.seek(0)    
+    
+
+
+    
