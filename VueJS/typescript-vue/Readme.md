77c6f862d7e94531bf3b95cbd07255de1932f617
Andrew Truex <andrewtruex@gmail.com>
2018-10-15 09:40:47 -0400

    Add Typescript Vue project files

--- VueJS/typescript-vue/Readme.md
+++ VueJS/typescript-vue/Readme.md
@@ -1,1 +1,26 @@
-Typescript Vue
+# ts-vue
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Run your tests
+```
+npm run test
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
---
+++ VueJS/typescript-vue/src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/retrotober.png">
+    <br>
+    <excited :name="name" :initialEnthusiasm="3"></excited>
+    <br>
+    <div class="container">
+      <form class="form-inline">
+        <div class="form-group">
+          <h2>
+            <label for="inputName" style="margin-right: 10px; margin-top: 4px;">Name:</label>
+          </h2>
+          <input
+            v-model="name"
+            type="text"
+            class="form-control form-control"
+            id="inputName"
+            aria-describedby="emailHelp"
+            placeholder="Enter name"
+          >
+        </div>
+      </form>
+    </div>
+    <br>
+    <hello-world msg="This is Your Vue.js + TypeScript App"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+// import HelloWorld from './components/HelloWorld.vue';
+import Excited from './components/Excited.vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+@Component({
+  data() {
+    return {
+      name: ""
+    }
+  },
+  components: {
+    Excited,
+    HelloWorld
+  }
+})
+
+export default class App extends Vue { }
+</script>
+
+<style>
+#app {
+	font-family: "Avenir", Helvetica, Arial, sans-serif;
+	/* font- */
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #47b683;
+}
+.container {
+	margin: auto;
+	max-width: 275px;
+}
+</style>
