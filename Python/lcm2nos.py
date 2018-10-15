481af5b302df7985fcff5b4a63d126515dbf0744
SakshamSaxena97 <sakshamsaxena1997@gmail.com>
2018-10-16 01:42:17 +0530

    Python lcm 2 nos

---
+++ Python/lcm2nos.py
@@ -0,0 +1,13 @@
+def lcm(x, y):
+   if x > y:
+       z = x
+   else:
+       z = y
+
+   while(True):
+       if((z % x == 0) and (z % y == 0)):
+           lcm = z
+           break
+       z += 1
+
+   return lcm
