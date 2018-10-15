e08a1d4be4495f7ab723c641e3f68b3ad4b17e17
Chris Deaton <flashadvocate@gmail.com>
2018-10-15 16:10:25 -0400

    Add slim index

---
+++ PHP/Slim/index.php
@@ -0,0 +1,18 @@
+<?php
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require 'vendor/autoload.php';
+
+$app = new \Slim\App;
+
+$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+    $name = $args['name'] ?? "World";
+    
+    $response->getBody()->write("Hello, {$name}!");
+
+    return $response;
+});
+
+$app->run();
