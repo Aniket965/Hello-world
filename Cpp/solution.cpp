26fbdc1bfde4045f0a8e925705e3da4dccf93333
kamleshmugdiya <kamleshmugdiya@gmail.com>
2018-10-15 22:41:33 +0530

    Hello, World!

---
+++ Cpp/solution.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define pll pair<ll, ll>
+#define vll vector<ll>
+#define vpll vector<pll >
+#define mp make_pair
+#define endl "\n"
+#define all(x) x.begin(), x.end()
+#define fastio ios::sync_with_stdio(false); cin.tie(0);
+#define T int tt; cin>>tt; while(tt--)
+
+int main()
+{
+    fastio;   
+    string hello = "Hello ,",world = "World"!;
+    cout<<hello<<world<<endl;
+    return 0; 
+}
