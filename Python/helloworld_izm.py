bd14fd8075963ad55d79d4b77a47ebcff825e161
Israel Zúñiga de la Mora <6629+israelzuniga@users.noreply.github.com>
2018-10-16 14:08:13 -0500

    Python/helloworld_izm.py

---
+++ Python/helloworld_izm.py
@@ -0,0 +1,14 @@
+##!/usr/bin/env python
+'''
+Israel Zuniga de la Mora
+'''
+import optparse
+import jaydebeapi as jdb
+
+
+def main():
+    world = 'World!'
+    print(f'Hello, {world}')
+
+if __name__ == '__main__':
+    main()
