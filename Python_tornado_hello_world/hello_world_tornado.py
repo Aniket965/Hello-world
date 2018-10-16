d8f456d54bcb87354a295c1c0cd427cf6e8c16e4
Gentle Yang <nodexy@qq.com>
2018-10-16 15:02:09 +0800

    add Python tornado hello world

---
+++ Python_tornado_hello_world/hello_world_tornado.py
@@ -0,0 +1,16 @@
+import tornado.ioloop
+import tornado.web
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello, world")
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+    ])
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
