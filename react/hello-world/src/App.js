a90caa884da54b60230bea6689ff874915045f6f
hoang <vinhyenvodoi98@gmail.com>
2018-10-16 22:40:37 +0700

    helloword with react first commit

---
+++ react/hello-world/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+     <div className="backgroup">
+      <div className="title">
+        <p className="title-hacktoberfest">Hacktoberfest-2018</p>
+      </div>
+      <div className="hello-world">
+        <p className="text-helloworld">hello world</p>
+      </div>
+     </div>
+    );
+  }
+}
+
+export default App;
