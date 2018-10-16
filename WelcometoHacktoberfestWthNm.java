77ad0facc9c70b32a4fdb0f3f60ca9765fd61516
Bcardoso2 <38514089+Bcardoso2@users.noreply.github.com>
2018-10-16 15:25:49 -0300

    WelcometoHacktoberfestWthNm.java
    It's a simple program with name of the user, and the Welcome 👍  (New on git)

---
+++ WelcometoHacktoberfestWthNm.java
@@ -0,0 +1,10 @@
+public class HelloHacktoberfest{
+
+    public static void main(String args[]){
+    Scanner sc = new Scanner (System.in);
+    String name;
+    System.out.prinln(" Write your name : ")
+      name = sc.nextLine();
+        System.out.println("Welcome to Hacktoberfest " + name + "!");
+    }
+}
