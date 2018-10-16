7d301dd3f265075b8bf6d8ddba1139899e2fd331
Aniruddha Nandi <anandi@buffalo.edu>
2018-10-16 19:07:39 -0400

    created hello_world_hacktoberfest.py

---
+++ Python/hello_world_hacktoberfest.py
@@ -0,0 +1,3 @@
+hello_world_ascii_list = [72, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100, 33, 32, 73, 116, 115, 32, 72, 97, 99, 107, 116, 111, 98, 101, 114, 102, 101, 115, 116]
+
+print(''.join(chr(i) for i in hello_world_ascii_list)) ## prints 'Hello world! Its Hacktoberfest'
