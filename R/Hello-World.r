1a38a1f38e75d4ca365bdc6ad780986e5fcdd484
Kourosh Banaeianzadeh <kourosh.banaeianzade@ucalgary.ca>
2018-10-16 09:14:00 -0600

    Create Hello World
    Describe how there are many ways to assign a variable in R.

---
+++ R/Hello-World.r
@@ -0,0 +1,9 @@
+#Assignment in R cand be done in many ways
+
+HelloWorldstring1 = "Hello World"
+HelloWorldstring2 <- "Hello World)
+
+print(HelloWorldstring1)
+print(HelloWorldstring2)
+
+#Results are the same
