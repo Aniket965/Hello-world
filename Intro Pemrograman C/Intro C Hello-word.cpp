5d853227a1c3d6945b41b081ca7eee739a70d7d8
Yogi Putra Pratama <34164415+yogiputrap@users.noreply.github.com>
2018-10-16 09:06:50 +0700

    Get start C#
    Get start C# for beginner!!

---
+++ Intro Pemrograman C/Intro C Hello-word.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/* Nama file: INTRO1.C */
+
+ main()
+{
+	printf("Assalamualaikum kawan!!!\n");
+	printf("Pemrograman aalah sesuatu yang menyenangkan");
+	printf("Dan Pemograman C adalah yang paling menyenangkan");
+	printf("Test ya?...\n.1\n..2\n...3\n");
+	getchar();
+}
