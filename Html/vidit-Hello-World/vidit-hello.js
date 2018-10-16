4e71297fde1e663927b466e7026cde208afba3ae
vidit <viditvarshney222@gmail.com>
2018-10-17 01:03:56 +0530

    Remove duplicated directory

--- Html/vidit-Hello-World/vidit-hello.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("btn").onclick = function() {
-    document.getElementById("btn").innerHTML = "Button Clicked";
-    document.getElementById("hello").innerHTML = "Hello-World";
-    document.getElementById("body").style.backgroundColor = "lightgreen";
-
-}
