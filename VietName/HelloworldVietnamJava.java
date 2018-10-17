a86f34cd63d3cce89fa03d902a2dac3caf16f37d
ledangtuanbk <ledangtuanbk@users.noreply.github.com>
2018-10-17 18:28:19 +0700

    Update HelloworldVietnamJava.java
    Print Name Vietnam Captial

--- VietName/HelloworldVietnamJava.java
+++ VietName/HelloworldVietnamJava.java
@@ -3,9 +3,6 @@ package test;
 public class HelloVietnameJava {
     public static void main(String[] args) {
         System.out.println("Xin Chào Việt Nam Java");
-        System.out.println("Xin Chào Hà Nội");
-        System.out.println("Xin Chào Bách Khoa Hà Nội");
-        System.out.println("Xin Chào Cầu Giấy District ");
-        System.out.println("Xin Chào Hoàn Kiếm ");
+        System.out.println("Xin Chào Hà Nội");        
     }
 }
