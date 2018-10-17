81264c19c1f8167ad2d099d338d931c56973a935
Lemoneydes <patje009@gmail.com>
2018-10-17 17:44:03 +0200

    C# Hallo Wereld

---
+++ c#/HalloWereld.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hallo Wereld");
+            Console.ReadLine();          
+        }
+    }
+}
