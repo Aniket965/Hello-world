9a263fe405a4059a77d9c706c033a0d1d3f6dfc5
tarandeep singh <44240513+taran9873@users.noreply.github.com>
2018-10-17 23:48:20 +0530

    simplified the variables and statements

--- C Language/Celsius_to_Fahrenheit.c
+++ C Language/Celsius_to_Fahrenheit.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 void main(){
-	printf("\n*****Covert the temperature from degree Celsius to degree Fahrenheit*****\n\n\n");
+	printf("\n**Covert the temperature from degree Celsius to degree Fahrenheit**\n");
 	
-	float oC;
+	float oC,F;
 	
-	printf("Enter degrees Celsius: ");
-	scanf("%f", &oC);
-
-	printf("--->Degrees Fahrenheit is: %.1f", oC * 1.8 + 32);
+	printf("Enter degrees Celsius:");
+	scanf("%f",&oC);
+         F=oC*1.8+32;
+	printf("fahrenhiet value is :%f",F);
 }
