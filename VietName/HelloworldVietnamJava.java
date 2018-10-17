509d86c9e7206ef9a2b9d10e0ad620d3561080ed
ledangtuanbk <ledangtuanbk@users.noreply.github.com>
2018-10-17 18:16:34 +0700

    Update HelloworldVietnamJava.java
    Xin Chào Hà Nội

--- VietName/HelloworldVietnamJava.java
+++ VietName/HelloworldVietnamJava.java
@@ -3,5 +3,6 @@ package test;
 public class HelloVietnameJava {
     public static void main(String[] args) {
         System.out.println("Xin Chào Việt Nam Java");
+        System.out.println("Xin Chào Hà Nội");
     }
 }
