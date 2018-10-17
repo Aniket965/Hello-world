65824715c0ba23e5943304e7063c89c24b2ed0ae
koronkowy-lacey <lacey.kasten@gmail.com>
2018-10-16 21:07:47 -0700

    Update Ruby/learnrubythehardwayex1helloworld.rb

---
+++ Ruby/learnrubythehardwayex1helloworld.rb
@@ -0,0 +1,7 @@
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Printing."
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
