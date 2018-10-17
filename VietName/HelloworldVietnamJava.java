736dfc897a53372157e1fedb395963db403d7bae
ledangtuanbk <ledangtuanbk@users.noreply.github.com>
2018-10-17 18:17:15 +0700

    Update HelloworldVietnamJava.java
    Xin Chào Bách Khoa Hà Nội

--- VietName/HelloworldVietnamJava.java
+++ VietName/HelloworldVietnamJava.java
@@ -4,5 +4,6 @@ public class HelloVietnameJava {
     public static void main(String[] args) {
         System.out.println("Xin Chào Việt Nam Java");
         System.out.println("Xin Chào Hà Nội");
+        System.out.println("Xin Chào Bách Khoa Hà Nội");
     }
 }
