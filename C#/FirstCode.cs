96780ce40bb88d7971b1e07697b74952b5949c37
Pawan Garia <Pawan.garia@gfk.com>
2018-10-17 15:05:54 +0200

    Write Hello world and open Console in Debug mode

---
+++ C#/FirstCode.cs
@@ -0,0 +1,15 @@
+using System;
+namespace HelloWorld
+{
+    class FirstCode 
+    {
+        static void Main() 
+        {
+            Console.WriteLine("Hello World!");
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
