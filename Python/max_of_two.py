8444d3aad6a8246cc2b357fe0f13abfca0669779
tarpitsahu <tarpitsahu@gmail.com>
2018-10-17 20:34:38 +0530

    Add files via upload

---
+++ Python/max_of_two.py
@@ -0,0 +1,11 @@
+def max_of_two(a,b):
+	if(a>b):
+		return a
+	else:
+		return b
+
+num1=input('Enter First Number : ')
+num2=input('Enter Second Number : ')
+print "Max of the two is : "
+print max_of_two(num1,num2)
+
