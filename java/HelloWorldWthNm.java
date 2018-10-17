c59bf2661f7135095dce7eaf7d120aae3a178bda
Bcardoso2 <38514089+Bcardoso2@users.noreply.github.com>
2018-10-17 08:52:18 -0300

    HelloWorldWthNm.java

---
+++ java/HelloWorldWthNm.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name;
+        System.out.println("Write your name: ");
+        name = sc.nextLine();
+        System.out.println("Hello, " + name + "!");
+    }
+    
+}
