4398ce1368d695f4bd23ff03d5b5f586506ebe4f
Pawan Garia <Pawan.garia@gfk.com>
2018-10-17 15:10:17 +0200

    Created Random String Generater util

---
+++ C#/RandomUtil.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+
+namespace Frontend_Automated_Tests.Util
+{
+    public class RandomUtil
+    {   
+        /// <summary>
+        /// Get the Random String.
+        /// </summary>
+        /// <returns></returns>
+        public static string GetRandomString()
+        {
+           return Path.GetRandomFileName();
+        }
+    }
+}
