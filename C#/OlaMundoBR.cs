b9a38d51909f47ed455b23d82d06341602d61320
Eudes Tenório <eudestfilho@gmail.com>
2018-10-17 14:56:27 -0300

    OlaMundoBR.cs
    Hello World in Portuguese Brazil!

---
+++ C#/OlaMundoBR.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace OlaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ola Mundo!");
+            Console.ReadKey();          
+        }
+    }
+}
