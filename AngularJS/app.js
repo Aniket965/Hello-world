5342bf789fd772ff71110714662ae6f04a7c0f6d
Daewon <42423575+hyundw84@users.noreply.github.com>
2018-10-17 11:24:20 +0900

    Update app.js

--- AngularJS/app.js
+++ AngularJS/app.js
@@ -1,6 +1,7 @@
 var app = angular.module("app", []);
     app.controller("HelloController", function($scope) {
       $scope.hello = "Hello World!";
+      $scope.helloworld = "Hello World";
 $scope.fibbo = function(numMax){
     for(var fibArray = [0,1], i=0,j=1,k=0; k<numMax;i=j,j=x,k++ ){
         x=i+j;
