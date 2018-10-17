36f265a28784c34d75ee1b4dded9138fc0b7fffc
tarandeep singh <44240513+taran9873@users.noreply.github.com>
2018-10-17 23:27:55 +0530

    added helpful comments for beginners

--- C Language/hi.c
+++ C Language/hi.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main() /* use void main() so that u don't need to return value */
 {
-printf("hi"); /* simple program for beginners for printing "hi" in c laguage */
+printf("hi"); /* simple program for beginners for printing hi in c laguage */
 
 return 0;
 }
