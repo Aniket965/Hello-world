84e47bb76c253aca30fac3b42c235ce7813f2677
tarandeep singh <44240513+taran9873@users.noreply.github.com>
2018-10-17 23:15:10 +0530

    added useful comments

--- C Language/hello.c
+++ C Language/hello.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-/* Hello */
+/* learning how to print Hello on screen */
 int main() 
 {
   printf("Hello-World");
-  return 0;
+  return 0;/* program must return 0 value if using int in main */
 }
