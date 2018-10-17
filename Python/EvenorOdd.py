a403df936f6c0cd29e184215facfc73a55b90594
mayank1729 <mbansal063@gmail.com>
2018-10-17 16:58:04 +0530

    A simple script whcih checks if a number is even or odd

---
+++ Python/EvenorOdd.py
@@ -0,0 +1,12 @@
+#Script in python 2
+
+#This checks if the given number is even or odd
+
+print "Enter a number"
+number = int(raw_input())
+
+if number%2==0:
+    print "Number is even"
+else:
+    print "Number is odd"
+
