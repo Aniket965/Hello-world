3bb419eb33dfed914d62483e7e1dc5bee4fb056c
Guillaume Brizolier <guillaume.brizolier@etu.emse.fr>
2018-10-17 18:02:07 +0200

    add a fun program that displays a snake

---
+++ C++/alonglongsnake.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main(void)
+{
+    cout << "Yo man! Watch this awesome snake !" << endl;
+
+    cout << "O" << endl;
+
+    for(int i = 0; i < 200; i++)
+    {
+        cout << "_";
+    }
+    cout <<"/" << endl;
+}
