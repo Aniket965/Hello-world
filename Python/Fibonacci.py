e1c25cc840e00af121bd04a8119ecff40f753a66
DanielXYZ2000 <daniel.schleicher@gmx.at>
2018-10-18 00:04:21 +0200

    small modifications and adding comments

--- Python/Fibonacci.py
+++ Python/Fibonacci.py
@@ -1,9 +1,10 @@
-#Doesn't work.
-import time
-
-fibonacci = [1, 1]
-n = int(input())
-while len(fibonacci) < n:
-    fibonacci.append(fibonacci[-1] + fibonacci[-2])
+# Fibunacci numbers
+# 
+fibonacci = [1, 1] # initialize the fibonacci vector
+print("How much numbers shall be calculated?:")
+n = int(input()) # read value
+while len(fibonacci) < n: # loop until the requested nr is calculated
+    fibonacci.append(fibonacci[-1] + fibonacci[-2]) # calculate the next fibonacci nr and add to the array
+# print all calculated numbers
 for i in range(n):
     print(fibonacci[i], end=' ')
