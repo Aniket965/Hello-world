2fd98a96a1781086392f263a716f236171fc4020
ASDF44 <42999582+ASDF44@users.noreply.github.com>
2018-10-17 08:58:34 +0530

    Update mergeSort.py

--- AlgoPy/mergeSort.py
+++ AlgoPy/mergeSort.py
@@ -1,42 +1,36 @@
-def merge(L,R,A):
-    nL = len(L) 
-    nR = len(R)
-    i = 0
-    j = 0
-    k = 0 
-    while(i < nL and j < nR):
-        if(L[i] <= R[j]):
-            A[k] = L[i]
-            i += 1
-            k += 1
-        else:
-            A[k] = R[j]
-            j += 1
-            k += 1
-    while(i<nL):
-        A[k] = L[i]
-        i += 1
-        k += 1
-    while(j<nR):
-        A[k] = R[j]
-        j += 1
-        k += 1
+def mergeSort(alist):
+    print("Splitting ",alist)
+    if len(alist)>1:
+        mid = len(alist)//2
+        lefthalf = alist[:mid]
+        righthalf = alist[mid:]
 
-def sort(A):
-    if(len(A) < 2):
-        return
-    mid = len(A) // 2
-    left = []
-    right = []
-    for i in range(0,mid):
-        left.append(A[i])
-    for i in range(mid,len(A)):
-        right.append(A[i])
-    sort(left)
-    sort(right)
-    merge(left,right,A)
+        mergeSort(lefthalf)
+        mergeSort(righthalf)
 
+        i=0
+        j=0
+        k=0
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i] < righthalf[j]:
+                alist[k]=lefthalf[i]
+                i=i+1
+            else:
+                alist[k]=righthalf[j]
+                j=j+1
+            k=k+1
 
-A = [2,4,1,6,8,5,3,7]
-sort(A)
-print(A)
+        while i < len(lefthalf):
+            alist[k]=lefthalf[i]
+            i=i+1
+            k=k+1
+
+        while j < len(righthalf):
+            alist[k]=righthalf[j]
+            j=j+1
+            k=k+1
+    print("Merging ",alist)
+
+alist = [54,26,93,17,77,31,44,55,20]
+mergeSort(alist)
+print(alist)
