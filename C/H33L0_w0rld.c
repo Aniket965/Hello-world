73ea3c3c5c33c6943273808af79a37de1c5fea98
yash bhansali <43386041+yash0906@users.noreply.github.com>
2018-10-18 15:12:46 +0530

    HELLO WORLD EXPLICITLY IN C
    HELLO WORLD USING ONLY C IN ONE LINE.

--- C/H33L0_w0rld.c
+++ C/H33L0_w0rld.c
@@ -1,7 +1,5 @@
 #include<stdio.h>
-
 int main()
 {
-	printf("Hello World");
-	return 0;
-  }
+	printf("C  C CCC C   C   CCC   C   C CCC CCC C   C\nC  C C   C   C   C C   C   C C C C C C   CC\nCCCC CCC C   C   C C   C C C C C CCC C   C C\nC  C C   C   C   C C   CC CC C C CC  C   CC\nC  C CCC CCC CCC CCC   C   C CCC C C CCC C\n");
+	return 0;}
