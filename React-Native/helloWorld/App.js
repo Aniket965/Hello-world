2bc5c9b23a58c99f272285fa5694abe2bc0c8d4a
fajarainul - dell <fajarainul14@gmail.com>
2018-10-18 14:28:50 +0700

    add simple calculator to hello world react-native

--- React-Native/helloWorld/App.js
+++ React-Native/helloWorld/App.js
@@ -8,6 +8,7 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import MainScreen from "./app/screen/MainScreen";
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -20,10 +21,11 @@ type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Hello World</Text>
-        <Text>Adding a random thing</Text>
-      </View>
+      {/*<View style={styles.container}>*/}
+        {/*<Text>Hello World</Text>*/}
+        {/*<Text>Adding a random thing</Text>*/}
+      {/*</View>*/}
+      <MainScreen/>
     );
   }
 }
--- React-Native/helloWorld/index.js
+++ React-Native/helloWorld/index.js
@@ -4,4 +4,4 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent('helloWorld', () => App);
