a3cd0fc1b53e1f1976349d6cc92ffccbbcf99964
Bhawana <sonachhaglani@gmail.com>
2018-10-18 20:39:54 +0530

    commited

--- C++/Factorial.cpp
+++ C++/Factorial.cpp
@@ -26,7 +26,7 @@ unsigned long long factorialLoop(int n)
 int main()
 {
     int n;
-    cout << "Enter a positive integer: ";
+    cout << "Enter a positive integer ";
     cin >> n;
     cout << "Factorial of " << n << " = " << factorial(n);
     return 0;
