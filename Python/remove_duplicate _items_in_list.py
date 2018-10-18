ea1231fcaea4b1f6b0e6062d7d2204df4c72d376
Sam Carani <scarani@users.noreply.github.com>
2018-10-17 22:44:09 -0500

    ADD: Better inline comments

--- Python/remove_duplicate _items_in_list.py
+++ Python/remove_duplicate _items_in_list.py
@@ -1,12 +1,12 @@
 # Remove duplicate items in a list
-
 def remove_duplicates(list_with_duplicates): 
     corrected_list = [] 
     for num in list_with_duplicates: 
         if num not in corrected_list: 
             corrected_list.append(num) 
     return corrected_list
-    
+
+# Test function
 old_list = [1,2,3,3,4]
 new_list = remove_duplicates(old_list)
 
